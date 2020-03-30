48b9830a8c3dbe11a9f4568b10869399b242b78f
Justin Pitts <pitts.justin@gmail.com>
2020-03-30 00:22:58 -0400

    working blog admin only

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class blog(models.Model):
+    title = models.CharField(max_length=200)
+    pubdate = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
